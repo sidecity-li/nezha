38b6592ed8beb84fc701ed281ad2e81957b718a3
sidecity.li <sidecity.li@osl.com>
2025-04-11 17:41:55 +0800

    feat: 添加按钮

---
+++ components/index.tsx
@@ -0,0 +1,2 @@
+export {Button} from './ui/button'
+import "./index.css"
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig, UserConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "."),
+export default defineConfig(() => {
+  const baseConfig: UserConfig = {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "."),
+      },
     },
-  },
-})
+    build: {
+      lib: {
+        entry: {
+          component: path.resolve(__dirname, "components/index.tsx"),
+        },
+        formats: ["es"],
+        fileName: (_, entryName) => `${entryName}.js`,
+        cssFileName: `component.css`,
+      },
+      rollupOptions: {
+        external: ['react', 'react/jsx-runtime', 'react-dom'],
+      },
+      sourcemap: true,
+    },
+  };
+  return {
+    mode: "development",
+    ...baseConfig,
+  };
+});
